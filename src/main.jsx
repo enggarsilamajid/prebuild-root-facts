1dd66e662a1506c66696739d5bc2fb088ed4e8b7
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-05-08 08:18:38 +0000

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,3 +6,23 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />,
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        '/sw.js',
+      );
+
+      console.log(
+        'Service Worker berhasil diregistrasi:',
+        registration.scope,
+      );
+    } catch (error) {
+      console.error(
+        'Service Worker gagal diregistrasi:',
+        error,
+      );
+    }
+  });
+}
